29e4825856c40d6ca91ca43a1f63dda0769adc6a
DQing <15829258737@163.com>
2018-07-11 13:52:14 +0800

    prime exceises
    Signed-off-by: DQing <15829258737@163.com>

---
+++ src/cn/thoughtworks/prime.java
@@ -0,0 +1,37 @@
+package cn.thoughtworks;
+
+import java.util.Scanner;
+
+/**
+ * Created by douqing on 18-7-11.
+ */
+public class prime {
+
+    public static void main(String[] args) {
+        System.out.print("n=");
+        Scanner n = new Scanner(System.in);
+        factor(n.nextInt());30
+    }
+
+    static void factor(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + "  ");
+                if (isPrime(number / i)) {
+                    System.out.print(number / i + "  ");
+                } else {
+                    factor(number / i);
+                }
+                return;
+            }
+        }
+    }
+    static boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
